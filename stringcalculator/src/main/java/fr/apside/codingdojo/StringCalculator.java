0110ec09529ad0833bcd319d0cf6ad9358c521af
Kevin Marcellier <k.marcellier@gmail.com>
2016-10-04 19:33:02 +0200

    Passage Marius

--- stringcalculator/src/main/java/fr/apside/codingdojo/StringCalculator.java
+++ stringcalculator/src/main/java/fr/apside/codingdojo/StringCalculator.java
@@ -1,21 +1,20 @@
 package fr.apside.codingdojo;
 
-public class StringCalculator {
+import java.util.Arrays;
 
+public class StringCalculator {
 
 	public static int add(String string) {
 		int result = 0;
-		if(!string.isEmpty()) {
+		if (!string.isEmpty()) {
 			result = sumString(string);
 		}
-		
+
 		return result;
 	}
 
 	private static int sumString(String string) {
-		int result;
-		result = Integer.valueOf(string);
-		return result;
+		return Arrays.asList(string.split(",")).stream().map(Integer::valueOf).reduce(0, (a, b) -> a + b);
 	}
 
 }
